17c0635a7147f57e8a6391008722fe8176ef33c4
Sherry Long <sherryl4@illinois.edu>
2026-09-23 15:49:40 -0500

    Problem 1.2

--- hw1/fgsm.py
+++ hw1/fgsm.py
@@ -17,12 +17,14 @@ N = nn.Sequential(nn.Linear(10, 10, bias=False),
 x = torch.rand((1,10)) # the first dimension is the batch size; the following dimensions the actual dimension of the data
 x.requires_grad_() # this is required so we can compute the gradient w.r.t x
 
-t = 0 # target class
+t = 1 # target class
 
-epsReal = 0.5  #depending on your data this might be large or small
+epsReal = 1.2  #depending on your data this might be large or small
 eps = epsReal - 1e-7 # small constant to offset floating-point erros
 
 # The network N classfies x as belonging to class 2
+print("Original Logits:", N(x))
+
 original_class = N(x).argmax(dim=1).item()  # TO LEARN: make sure you understand this expression
 print("Original Class: ", original_class)
 assert(original_class == 2)
@@ -37,6 +39,7 @@ loss.backward()
 # adv_x should be computed from x according to the fgsm-style perturbation such that the new class of xBar is the target class t above
 # hint: you can compute the gradient of the loss w.r.t to x as x.grad
 adv_x = x - eps * x.grad.sign()
+print("New Logits:", N(adv_x)) 
 
 new_class = N(adv_x).argmax(dim=1).item()
 print("New Class: ", new_class)
